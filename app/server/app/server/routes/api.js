450e01da118fa60f7bccac08aa83b8e00b4a113c
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2026-02-18 07:39:18 -0500

    HMW-1000 Added error message to configFiles and supportedBrowsers api endpoints.

--- app/server/app/server/routes/api.js
+++ app/server/app/server/routes/api.js
@@ -2,9 +2,15 @@ const express = require('express');
 const updateConfigCache = require('../../tasks/updateConfigCache');
 const configCache = require('../utilities/configCache');
 
-async function sendCachedData(res, key) {
+async function sendCachedData(res, key, message) {
   const data = configCache.get(key);
-  if (!data) await updateConfigCache();
+  if (!data) {
+    try {
+      await updateConfigCache();
+    } catch (err) {
+      res.status(500).json({ message });
+    }
+  }
 
   res.set({
     'Content-Type': 'application/json',
@@ -22,12 +28,12 @@ module.exports = function (app) {
 
   // --- get static content from S3
   router.get('/configFiles', (req, res) => {
-    sendCachedData(res, 'configFiles');
+    sendCachedData(res, 'configFiles', 'Failed to get config file cache...');
   });
 
   // --- get static content from S3
   router.get('/supportedBrowsers', (req, res) => {
-    sendCachedData(res, 'supportedBrowsers');
+    sendCachedData(res, 'supportedBrowsers', 'Failed to get supported browsers cache...');
   });
 
   router.use((req, res) => {
